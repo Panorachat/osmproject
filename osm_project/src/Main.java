00eac0e65556546c7ace38d698b47c390101fef6
hilairef <hilairef@hilairef-300E4A-300E5A-300E7A-3430EA-3530EA>
2015-12-21 15:20:02 +0100

    Test Flo

--- osm_project/src/Main.java
+++ osm_project/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
-
+//test Flo
                 Dessin ex = new Dessin();
                 ex.setVisible(true);
             }
