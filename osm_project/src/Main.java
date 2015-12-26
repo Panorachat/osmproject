5b25626938d33c8cc6a8972c61cfeecd8526897d
Mounir <Mounir@Mounir-PC>
2015-12-26 02:49:11 +0000

    test

--- osm_project/src/Main.java
+++ osm_project/src/Main.java
@@ -5,7 +5,6 @@ public class Main {
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
-
                 Dessin ex = new Dessin();
                 ex.setVisible(true);
             }
