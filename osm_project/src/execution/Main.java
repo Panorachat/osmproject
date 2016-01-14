d6c91910644c2be6f4a2035c41eba7ec51221268
Mounir <Mounir@192.168.1.44>
2016-01-14 16:21:53 +0100

    EventQueue ne sert à rien, on garde les codes simples c'est mieux !

--- osm_project/src/execution/Main.java
+++ osm_project/src/execution/Main.java
@@ -1,16 +1,9 @@
 package execution;
-import java.awt.EventQueue;
-
 import GUI.Dessin;
 
 public class Main {
 	public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                Dessin ex = new Dessin();
-                ex.setVisible(true);
-            }
-        });
+		Dessin fenetre = new Dessin();
+		fenetre.setVisible(true);
 	}
 }
